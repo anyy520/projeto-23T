7132dcaac2d72bb7137cd48b320f5f5690d1a5ea
AnyySann <86086338+anyy520@users.noreply.github.com>
2023-12-06 11:20:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,82 +1,115 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-// 3
-var btn2;
-//5  Vamos criar uma variável de ângulo e defini-la com um valor aleatório de 60
-var angle=60;
 
+var engine, world;
 
-function setup() {
-  createCanvas(400,400);
+var particle1, particle2,particle3,particle4,particle5;
+var particle6, particle7,particle8,particle9,particle10;
+var rotator1, rotator2, rotator3;
+var block1, block2;
 
-  engine = Engine.create();
-  world = engine.world;
+var angle1=60;
+var angle2=60;
+var angle3=60;
+
+function setup(){
   
-   var ball_options = {
-    // usada para fazer um corpo quicar no chão +=+
-    restitution: 0.95,
-    // atrito de ar + = -
-    frictionAir:0.01
-  }
-   
-   var ground_options ={
-     isStatic: true
-   };
-  // 3 Crie um botão na tela e anexe essas funções ao botão
-  btn2 = createImg('up.png');
-  btn2.position(350,30);
-  btn2.size(50,50);
-  btn2.mouseClicked(vForce);
-// 4. Verifique a instrução abaixo no arquivo sketch.js e altere os valores do retângulo.
-  ground1 = Bodies.rectangle(100,300,100,20,ground_options);
-  World.add(world,ground1);
+    var canvas = createCanvas(550,600);
+    engine = Engine.create();
+    world = engine.world;
 
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
-  
- 
-  ground = Bodies.rectangle(100,400,650,20,ground_options);
-  World.add(world,ground); 
-  
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
+    // 3 crie corpos planos e blocos
+    var plane_options={
+      isStatic: true
+    }
+
+    plane = Bodies.rectangle(600,height,1200,20,plane_options);
+    World.add(world,plane);
+    block1=Bodies.rectangle(100,400,150,20,plane_options);
+    World.add(world,block1);
+    block2=Bodies.rectangle(400,400,150,20,plane_options);
+    World.add(world,block2);
+
+    //crie múltiplos de corpos de partículas
+    var particle_options = {
+      restitution:0.4,
+      friction:0.02
+    }
+
+    particle1 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle1);
+    particle2 = Bodies.circle(220,10,10,particle_options);
+    World.add(world,particle2);
+    particle3 = Bodies.circle(225,10,10,particle_options);
+    World.add(world,particle3);
+    particle4 = Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle4);
+    particle5 =Bodies.circle(230,10,10,particle_options);
+    World.add(world,particle5);
+
+    
+    //crie todos os três corpos de rotadores
+    var rotator_options={
+      isStatic:true
+    };
+
+    rotator1 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator1);
+
+    rotator2 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator2);
+
+    rotator3 = Bodies.rectangle(250,200,150,20,rotator_options);
+    World.add(world,rotator3);
+
+    //estilizando os corpos aqui
+    fill("brown");
+    rectMode(CENTER);
+    ellipseMode(RADIUS);
+
 }
 
+function draw(){
+    background("lightgreen");
+  //2  Chame update() para atualizar o mecanismo de física em draw().
+    Engine.update(engine);
 
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  
-  // 6 Adicione o código para girar o objeto dentro da função draw(). 
-  Matter.Body.rotate(ground1,angle);
-  //8  Gire o solo adicionando as funções push() e pop() dentro da função draw():
+  //crie forma de plano e suporte
+  rect(plane.position.x,plane.position.y,1200,20);
+  rect(block1.position.x,block1.position.y,150,20);
+  rect(block2.position.x,block2.position.y,150,20);
 
+  //crie forma para todas as partículas
+  ellipse(particle1.position.x,particle1.position.y,10);
+  ellipse(particle2.position.x,particle2.position.y,10);
+  ellipse(particle3.position.x,particle3.position.y,10);
+  ellipse(particle4.position.x,particle4.position.y,10);
+  ellipse(particle5.position.x,particle5.position.y,10);
+
+  //crie forma para todos os rotadores
+  Matter.Body.rotate(rotator1,angle1)
   push();
-  translate(ground1.position.x,ground1.position.y);
-  rotate(angle);
-  rect(0,0,100,20);
+  translate(rotator1.position.x,rotator1.position.y);
+  rotate(angle1);
+  rect(0,0,150,20);
   pop();
-  
-  // 7 . Aumente o ângulo da rotação em 0.1 graus dentro da função draw.
-  angle +=0.1;
+  angle1 +=0.2;
 
- 
-// Exibe a bola passando sua posição na função elipse.
-  ellipse(ball.position.x,ball.position.y,20);
-  rect(ground.position.x,ground.position.y,650,20);
- 
-console.log(ground.position.y);
+  Matter.Body.rotate(rotator2,angle2)
+  push();
+  translate(rotator2.position.x,rotator2.position.y);
+  rotate(angle2);
+  rect(0,0,150,20);
+  pop();
+  angle2 +=0.3;
 
-  
-  
+  Matter.Body.rotate(rotator3,angle3)
+  push();
+  translate(rotator3.position.x,rotator3.position.y);
+  rotate(angle3);
+  rect(0,0,150,20);
+  pop();
+  angle3 +=0.4;
+    
 }
-// 1. Crie uma função chamada vForce(), pois esta função aplicará força na direção vertical.
-function vForce()
-{
-// 2. Use a função em Matter.js para aplicar força ao corpo
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05});
-}
-
